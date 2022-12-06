8e422c2feeacaf991eaa47d26bf0c65820801ebe
Fan Kangtai <fankangtai@gmail.com>
2022-12-06 12:30:04 +0800

    Exercise 1: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import {QUERIES} from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -29,7 +29,23 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>
         <Logo />
+        <SubscribeActionGroup>
+          <Button>
+            Subscribe
+          </Button>
+          <LoginLink>
+            Already a subscriber?
+          </LoginLink>
+        </SubscribeActionGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +55,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+  
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +79,41 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const SubscribeActionGroup = styled(DesktopActionGroup)`
+  flex-direction: column;
+  gap: 0;
+  align-items: center;
+  align-self: end;
+`;
+
+const LoginLink = styled.a`
+  color: var(--color-gray-900);
+  font-weight: var(--font-weight-normal);
+  font-style: italic;
+  font-family: var(--font-family-serif);
+  text-decoration: underline;
+`;
+
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  
+  @media ${QUERIES.laptopAndUp} {  
+    justify-content: space-between; 
+    margin-top: 16px;
+  }
 `;
 
 export default Header;
