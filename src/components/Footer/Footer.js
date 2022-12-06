ac83a13750fd2cfff78ffcca7a1a2e9389c55407
Fan Kangtai <fankangtai@gmail.com>
2022-12-06 18:14:39 +0800

    Exercise 5: Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import VisuallyHidden from '../VisuallyHidden';
+import {QUERIES} from "../../constants";
 
 const Footer = () => {
   return (
@@ -26,15 +27,15 @@ const Footer = () => {
           <Social>
             <a href="/">
               <VisuallyHidden>
-                Visit The Grid Times on Facebook
-              </VisuallyHidden>
-              <Facebook size={20} />
-            </a>
-            <a href="/">
-              <VisuallyHidden>
                 Visit The Grid Times on Twitter
               </VisuallyHidden>
               <Twitter size={20} />
+            </a>
+            <a href="/">
+              <VisuallyHidden>
+                Visit The Grid Times on Facebook
+              </VisuallyHidden>
+              <Facebook size={20} />
             </a>
           </Social>
         </TopRow>
@@ -144,6 +145,16 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+  
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: row;
+    justify-content: center;
+    gap: 32px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: end;
+  }
 `;
 
 const Social = styled.div`
@@ -170,6 +181,13 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+  
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, 240px);
+    justify-content: space-between;
+    text-align: start;
+  }
 `;
 
 const MainNavHeading = styled.h2`
@@ -196,6 +214,10 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  
+  @media ${QUERIES.laptopAndUp} {
+    align-items: start;
+  }
 `;
 
 const Logo = styled.a`
